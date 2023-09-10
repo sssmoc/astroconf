d553ee1518412ad768574e7f89dd1800b4677969
sssmoc <sebasdriven15@gmail.com>
2023-09-10 18:44:46 -0500

    Nvchad telescope theme and julia-vim pligin

--- plugins/user.lua
+++ plugins/user.lua
@@ -12,14 +12,15 @@ return {
   },
 
   {
+    "JuliaEditorSupport/julia-vim",
+    event = "User AstroFile",
+  },
+
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
+    config = function() require("nvim-surround").setup {} end,
   },
   --
   -- "andweeb/presence.nvim",
