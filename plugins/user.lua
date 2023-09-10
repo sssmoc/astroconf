01e1bfe6c8e9dd99b5072a0f6a76df7792ec309a
sssmoc <sebasdriven15@gmail.com>
2023-09-10 16:33:51 -0500

    plugins configured

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,16 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    keys = {
+      { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Open TODO's in Telescope" },
+    },
+  },
+  --
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
